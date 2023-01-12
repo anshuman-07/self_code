162a5b7d9bc8fabfe3789b1b18b6b367b521bae0
abiswal <anshuman.biswal@mavenir.com>
2023-01-12 09:06:44 +0530

    second commit

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,34 @@
-public class Controller {
+import java.io.IOException;
 
-    public static String player1 = "";
-    public static String player2 = "";
-    public boolean HandleInput( int pos){
-        return false;
+public class Controller {
+    private static String GAME_INTRO = "Welcome to TicTacToe.";
+    private static String PLAYER1_NAME_PROMPT = "Please enter player1 name.";
+    private static String PLAYER2_NAME_PROMPT = "Please enter player2 name.";
+    public static void main(String[] args) throws IOException {
+        System.out.println(GAME_INTRO);
+        System.out.println(PLAYER1_NAME_PROMPT);
+        byte byte1[]=new byte[50];
+        System.in.read(byte1);
+        String player1Name =new String(byte1).trim();
+        System.out.println(PLAYER2_NAME_PROMPT);
+        byte byte2[]=new byte[50];
+        System.in.read(byte2);
+        String player2Name =new String(byte2).trim();
+        System.out.println("For "+player1Name + ", input is X");
+        System.out.println("For "+player2Name + ", input is 0");
+        System.out.println("Please enter a position in the given dashboard.");
+        TicTacToe game = new TicTacToe(player1Name,player2Name);
+        game.displayBoard();
+        while (game.hasNextTurn()){
+            if(game.player1Turn)
+                System.out.println("Input for " + player1Name + ": ");
+            else
+                System.out.println("Input for " + player2Name + ": ");
+            byte inputByte[]=new byte[50];
+            System.in.read(inputByte);
+            String input =new String(inputByte).trim();
+            game.playTurn(input);
+
+        }
     }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,16 +1,173 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class TicTacToe {
 
-    private static Controller controller = null;
-    public static void main(String[] args) {
-        controller = new Controller();
-        Controller.player1 = "X";
-        Controller.player2 = "0";
-        System.out.println("Hello , game started.");
-        System.out.println("Player1 = X");
-        System.out.println("Player2 = 0");
-    }
+   public boolean player1Turn = true;
+   String[][] board = new String[3][3];
+   public TicTacToe(String player1 , String player2){
+      int counter = 1;
+      for(String[] array : board){
+         int i = 0;
+         for(String c : array){
+             c = Integer.toString(counter);
+             array[i]= c;
+             i++;
+             counter++;
+         }
+         i = 0;
+      }
+   }
 
-    public void addInput(int pos){
-        controller.HandleInput(pos);
-    }
+   public void playTurn(String position){
+      updateBoard(position);
+      isPlayerWon(position);
+      player1Turn = !player1Turn;
+   }
+
+   public boolean hasNextTurn(){
+      for(String[] array : board){
+         for(String c : array){
+            if(!(c.equals("X") || c.equals("0")))
+               return true;
+         }
+      }
+      System.out.println("Game tied.");
+      return false;
+   }
+
+   private boolean isPlayerWon(String pos){
+      switch (pos){
+         case "1":
+         {
+            if(player1Turn && (board[1][1].equals("X") && board[2][2].equals("X")) ||
+                    (board[0][1].equals("X") && board[0][2].equals("X")) ||
+                    (board[1][0].equals("X") && board[2][0].equals("X"))) {
+               System.out.println("player1 won.");
+            }else if((board[1][1].equals("0") && board[2][2].equals("0")) ||
+                    (board[0][1].equals("0") && board[0][2].equals("0")) ||
+                    (board[1][0].equals("0") && board[2][0].equals("0"))){
+               System.out.println("player2 won.");
+            }
+         }
+            break;
+         case "2": {
+            if((board[0][0].equals("X")&&board[2][2].equals("X"))||
+                    (board[0][1].equals("X")&&board[0][2].equals("X"))||
+                    (board[1][0].equals("X")&&board[2][0].equals("X"))){
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+         case "3":
+         {
+            if (board[1][1].equals("X") && board[2][2].equals("X")) {
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+         case "4":
+         {
+            if (board[1][1].equals("X") && board[2][2].equals("X")) {
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+         case "5":
+         {
+            if (board[1][1].equals("X") && board[2][2].equals("X")) {
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+         case "6":
+         {
+            if (board[1][1].equals("X") && board[2][2].equals("X")) {
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+         case "7":
+         {
+            if (board[1][1].equals("X") && board[2][2].equals("X")) {
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+         case "8":
+         {
+            if (board[1][1].equals("X") && board[2][2].equals("X")) {
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+         case "9":
+         {
+            if (board[1][1].equals("X") && board[2][2].equals("X")) {
+               if (player1Turn) {
+                  System.out.println("player1 own");
+               } else {
+                  System.out.println("player2 own");
+               }
+            }
+         }
+            break;
+      }
+      return false;
+   }
+
+   private void updateBoard(String input){
+      for(String[] array : board){
+         int i = 0;
+         for(String c : array){
+            if(input.equals(c)){
+               array[i]=  player1Turn ? "X" : "0";
+            }
+            i++;
+         }
+         i = 0;
+      }
+      displayBoard();
+   }
+
+   public void displayBoard(){
+      for(String[] array : board){
+         for(String c : array){
+            System.out.print(c + " " + "| ");
+         }
+
+         System.out.println();
+         System.out.print("--  --  --");
+         System.out.println();
+      }
+   }
+
+
 }
